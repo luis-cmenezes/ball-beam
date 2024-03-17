9ad5224ccbbdc04e529a7d5bedd45dd8149271b5
Luis Costa <lipemenezescosta@gmail.com>
2024-03-16 21:20:43 -0300

    feat(docker): New dockerfile with arduino ide and sh to run it.

---
+++ utils/run-ball-beam-dev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+XSOCK=/tmp/.X11-unix
+
+# Allows root
+xhost +SI:localuser:root
+
+# Run, in a separate non-interactive terminal, micro ros agent
+gnome-terminal -- docker run --rm -v /dev:/dev \
+                    --privileged --net=host \
+                    --name micro_ros_agent \
+                    microros/micro-ros-agent:humble \
+                    serial --dev /dev/ttyUSB0 -v6
+
+# Run, interactively in current terminal, ball-bean container
+docker run -it --rm --net=host \
+         -v $XSOCK:$XSOCK:ro \
+         -v /dev:/dev \
+         -e DISPLAY=$DISPLAY \
+         --privileged \
+         ball-beam:latest  
+
+docker stop micro_ros_agent
+
+# Unallows root
+xhost -SI:localuser:root
